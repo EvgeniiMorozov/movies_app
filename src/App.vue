a516737e9b269deba451e50f43ac46b6e2aaf394
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-20 10:25:56 +0500

    lesson 3

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div>
+      <b-button-group>
+        <b-button>Button 1</b-button>
+        <b-button>Button 2</b-button>
+        <b-button>Button 3</b-button>
+      </b-button-group>
+    </div>
+    <div class="mt-3">
+      <b-button-group>
+        <b-button variant="success">Success</b-button>
+        <b-button variant="info">Info</b-button>
+        <b-button variant="warning">Warning</b-button>
+      </b-button-group>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
 
