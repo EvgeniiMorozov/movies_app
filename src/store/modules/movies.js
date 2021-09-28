a4fa0c09660d6e57c33850151ad735c5773c0167
Evgenii Morozov <zheka1101@yandex.ru>
2021-09-28 16:53:37 +0500

    lesson 5.1

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,15 +1,30 @@
 import axios from "@/plugins/axios";
+import IDs from "@/store/mock/imdb_top250";
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
+  state: {
+    top250IDs: IDs,
+    moviesPerPage: 12,
+    currentPage: 1,
+  },
+  getters: {
+    slicedIDs:
+      ({ top250IDs }) =>
+      (from, to) =>
+        top250IDs.slice(from, to),
+    currentPage: ({ currentPage }) => currentPage,
+    moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+  },
   mutations: {},
   actions: {
-    async fetchMovies(context) {
-      console.log(context);
-      //  tt0111161
-      const response = await axios.get("/?i=tt0111161");
+    async fetchMovies({ getters }) {
+      const { currentPage, moviesPerPage, slicedIDs } = getters;
+      const from = currentPage * moviesPerPage - moviesPerPage;
+      const to = currentPage * moviesPerPage;
+      const moviesToFetch = slicedIDs(from, to);
+      const requests = moviesToFetch.map((id) => axios.get(`/?=${id}`));
+      const response = await Promise.all(requests);
       console.log(response);
     },
   },
